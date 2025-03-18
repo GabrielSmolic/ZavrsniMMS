66e52f5e46ee05bd080e666d172595411e08eb22
GabrielSmolic <117285146+GabrielSmolic@users.noreply.github.com>
2025-03-18 10:15:06 +0100

    Delete ZavrsniMMS-main directory

--- ZavrsniMMS-main/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.carousel').forEach((carousel) => {
-    carousel.addEventListener('slid.bs.carousel', (event) => {
-      window.scrollTo({ top: window.scrollY, behavior: 'instant' });
-    });
-  });
-  
